c040e2dac41a492ccaee4a9543d5f0fc16519d3d
avershel <austinv@digitalinfuzion.com>
2015-07-01 10:29:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,63 +1,28 @@
-
-// window.onload = function () {
-var socket = io();
-
+var socket = io.connect('http://nolo-tr.elasticbeanstalk.com');
+// var socket = io();
 var holder = [];
 var xVal = 0;
 var yVal = 0;
 var updateInterval = 5000;
 var dataLength = 60; // number of dataPoints visible at any point
-// console.log('here');
-var term;
-// console.log('here');
+
+
+
+
 socket.on('foo', function(msg) {
-		// console.info(msg);
-		// console.log('here');
-
-		// console.log(msg.length);
-		// yVal = msg.length;
-		 d = new Date();
-		 l0 = d.getHours();
-		l1 = d.getMinutes();
-		l2 = d.getSeconds();
-    // xVal = String(l0 + ':' + l1 + ":" + l2);
-		// console.log(xVal);
-		// updateChart();
-		// len = 0;
-
-});
-socket.on('tweet', function(msg) {
-  // if (msg.text) {
-  //   var a = msg.text;
-  //   if (term == null || term.length == 0) {
-  //     holder.push(msg);
-  //
-  //   }
-  //   else {
-  //     // var x = msg[text];
-  //     // console.log(x);
-  //       if (a.indexOf(term) > -1) {
-  //         console.log(a);
-  //         holder.push(msg);
-  //
-  //       }
-  //
-  //
-  //   }
-  // }
   holder.push(msg);
-  // console.log(msg);
-
-
 });
+
+
 
 
 function updateterm()
 {
   console.log('updating term');
-  term=document.getElementById("tf").value;
+  var term=document.getElementById("tf").value;
   console.log('term = ' + term);
-  socket.emit('join', JSON.stringify({}));
+  holder = [];
+  socket.emit('term', term);
 
 }
 var dps = []; // dataPoints
@@ -116,23 +81,3 @@ setInterval(function() {
   console.log(holder.length);
   holder = [];
 }, 1000);
-
-
-
-/*
- Connect to socket.io on the server.
- */
-// var host = window.location.host//.split(':')[0];
-// var socket = io.connect('http://' + host, {reconnect:false, 'try multiple transports':false});
-// var intervalID;
-// var reconnectCount = 0;
-
-
-//
-// $.post('/user', {"user":name})
-//     .success(function () {
-//         // send join message
-//         socket.emit('join', JSON.stringify({}));
-//     }).error(function () {
-//         console.log("error");
-//     });
